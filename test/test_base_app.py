0ca38fe293a20cdee932574454c586f3ac5a966c
tomo <tom.maczkowski@gmail.com>
2024-09-28 12:48:45 +0200

    added minimal app with tests

---
+++ test/test_base_app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import pytest
+
+from app import base_app
+
+def add(a: int, b: int) -> int: return a + b
+def div(a: int, b: int) -> int: return a // b
+
+class TestApp:
+    def test_add(self):
+        app = base_app.BaseApp(2, 3)
+        assert app.do_action(add) == 5
+
+    def test_exception(self):
+        app = base_app.BaseApp()
+        with pytest.raises(TypeError):
+            app.do_action(3)
+
+    def test_div0(self):
+        app = base_app.BaseApp(2, 0)
+        with pytest.raises(ZeroDivisionError):
+            app.do_action(div)
